5d6dc8787974893ba1fd35fc1749011d96b05276
a-llt <nyyn4789@gmail.com>
2025-04-11 18:37:21 -0300

    fix resolução

--- count_symmetric_numbers.py
+++ count_symmetric_numbers.py
@@ -1,12 +1,15 @@
 
 def sym(low,high):
+    def num_len(n):
+        return len(str(n))
+    
     count = 0
     for n in range(low,high+1):
-        if len(str(n))%2==0:
-            sumL = sum(int(digit) for digit in str(n)[:len(str(n))//2])
-            sumR = sum(int(digit) for digit in str(n)[len(str(n))//2:])
+        if num_len(n)%2==0:
+            s = str(n)
+            mid = len(str(n))//2
+            sumL = sum(int(digit) for digit in s[:mid])
+            sumR = sum(int(digit) for digit in s[mid:])
             if sumL == sumR:
                 count += 1
     return count
-
-print(sym(1,100))
